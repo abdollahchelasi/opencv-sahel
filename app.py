ffbd64348f91207795ed7d8b81c6829ece2d3567
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2024-01-10 20:08:43 +0330

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import easyocr
+import streamlit as st
+
+upload = st.file_uploader("Choose a file")
+if upload is not None:
+    byte = upload.getvalue()
+    reader = easyocr.Reader(['fa','en'],gpu=False)
+    result = reader.readtext(byte,detail=0,paragraph=True,decoder='beamsearch')
+    for i in result:
+        st.info(i)
