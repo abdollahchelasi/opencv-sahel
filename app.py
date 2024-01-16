df7d597e2389d6510fb7080875c0d4a16ce71b62
abdollah <32410133+abdollahchelasi@users.noreply.github.com>
2024-01-16 17:22:33 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import easyocr
 import streamlit as st
 
-upload = st.file_uploader("Choose a file")
+upload = st.file_uploader("Choose a file",type=["png","jpg","jpeg"])
 if upload is not None:
     byte = upload.getvalue()
     reader = easyocr.Reader(['fa','en'],gpu=False)
     result = reader.readtext(byte,detail=0,paragraph=True,decoder='beamsearch')
     for i in result:
         st.info(i)
+
